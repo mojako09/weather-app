0f3bd5eb510022fb72f62798fd4a18f25d01192e
Matthew Ervin Silvestre <matt09silvestre@gmail.com>
2022-10-01 10:22:33 +0800

    adding div

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react'
+import axios from 'axios'
 
 function App() {
+
+// const url=`https://api.openweathermap.org/data/2.5/weather?q=dallas&appid=e12ad7ae69caca40419bf561964228cb`
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="container">
+        <div className="top">
+          <div className="location">
+            <p>Manila</p>
+          </div>
+          <div className="temp">
+              <h1>60F</h1>
+          </div>
+          <div className="description">
+            <p>Clouds</p>
+          </div>
+        </div>
+          <div className="bottom">
+            <div className="feels">
+              <p>65F</p>
+              <div className="humidity">
+                12mph
+              </div>
+            </div>
+          </div>
+      </div>
     </div>
   );
+
 }
 
 export default App;
